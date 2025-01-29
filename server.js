fcd11390d0f8aa0f90c1ffe318035ac51dcf7a71
Axpex <85782371+Axpex2@users.noreply.github.com>
2025-01-29 17:01:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const WebSocket = require('ws');
 const express = require('express');
 const http = require('http');
+const path = require('path');
 
 const PORT = process.env.PORT || 10000;
 const app = express();
 
-// HTTP-сервер для Render
+// Роздача статичних файлів (щоб HTML відкривався)
+app.use(express.static(path.join(__dirname, "public")));
+
+// HTTP-сервер
 const server = http.createServer(app);
 
-// WebSocket-сервер на тому ж порту
+// WebSocket-сервер
 const wss = new WebSocket.Server({ server });
 
 app.get('/', (req, res) => {
-    res.send('WebSocket server is running');
+    res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 wss.on('connection', ws => {
@@ -32,7 +36,7 @@ wss.on('connection', ws => {
     });
 });
 
-// Сервер слухає порт, який видає Render
+// Запуск сервера
 server.listen(PORT, () => {
     console.log(`Сервер запущено на порту ${PORT}`);
 });
