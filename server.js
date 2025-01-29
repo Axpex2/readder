5945aa033b44189debe4d1555ed128cfd31ba6cb
Axpex <85782371+Axpex2@users.noreply.github.com>
2025-01-29 02:38:36 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({ port: 3000 });
+
+server.on('connection', socket => {
+    console.log('Новий користувач підключився');
+
+    socket.on('message', message => {
+        console.log('Отримано повідомлення:', message);
+        server.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    socket.on('close', () => {
+        console.log('Користувач відключився');
+    });
+});
+
+console.log('Сервер WebSocket запущено на ws://localhost:3000');
